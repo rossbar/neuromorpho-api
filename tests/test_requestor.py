e0ba2cc42b1f7380e938d172f222aec7f643dbff
Ross Barnowski <rossbar@caltech.edu>
2023-09-15 00:15:14 -0700

    Add xfailed test for default ssl context.

--- tests/test_requestor.py
+++ tests/test_requestor.py
@@ -8,6 +8,10 @@ These are essentially smoke tests, i.e. we're testing for the absence of
 NOTE: These tests actually make GET requests to the server! Good citizenship
 dictates the tests should be run sparingly.
 """
+import pytest
+import requests
+from requests.exceptions import SSLError
+
 from neuromorpho_api import requestor
 
 
@@ -15,3 +19,12 @@ def test_neuron_endpoint():
     resp = requestor.get("https://neuromorpho.org/api/neuron/fields")
     assert resp.status_code == 200
     assert "Neuron Fields" in resp.json()
+
+
+@pytest.mark.xfail(raises=SSLError, strict=True)
+def test_default_ssl_context():
+    """
+    This is expected to fail due to the DH key for the neuromorpho.org cert.
+    If this starts passing, there's no need for the custom ssl context anymore.
+    """
+    resp = requests.get("https://neuromorpho.org/api/neuron/fields")
