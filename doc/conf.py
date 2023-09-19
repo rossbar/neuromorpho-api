96574a0761c1c86e752e3497381eabeb979fc300
Ross Barnowski <rossbar@caltech.edu>
2023-09-19 16:37:30 -0700

    DOC: Update website links.

--- doc/conf.py
+++ doc/conf.py
@@ -17,11 +17,36 @@ extensions = ["myst_nb"]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
 html_title = "neuromorpho-api"
-#html_static_path = ['_static']
+html_theme_options = {
+    "external_links": [
+        {
+            "url": "https://neuromorpho.org/",
+            "name": "Neuromorpho.org",
+        },
+    ],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/rossbar/neuromorpho-api",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/neuromorpho-api/",
+            "icon": "fas fa-box",
+        },
+    ],
+    "use_edit_page_button": True,
+}
+
+html_context = {
+    "github_user": "rossbar",
+    "github_repo": "neuromorpho-api",
+    "github_version": "main",
+    "doc_path": "doc",
+}
